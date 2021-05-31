5b1cefcd9d9806c2a229295762c866a193ab4709
sflynn <santiago.flynn2@gmail.com>
2021-05-31 15:34:20 -0300

    push

--- src/Screens/Tabs/HomeScreen.js
+++ src/Screens/Tabs/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Tabnav } from 'react-native';
+
 function HomeScreen ({navigation}) {
     return (
         <View style={styles.container}>
